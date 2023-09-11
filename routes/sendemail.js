d398d86505bfe836ebbe9bbed34a4fead71ca74c
teotimepacreau <95431443+teotimepacreau@users.noreply.github.com>
2023-09-11 18:42:36 +0200

    several UI changes for the email, added local fonts

--- routes/sendemail.js
+++ routes/sendemail.js
@@ -22,43 +22,70 @@ async function getSubscribers(){
     return subscribers
 }
 
-(async ()=>{
-    try{
-        const subscribers = await getSubscribers()
-        console.log(subscribers)
+(async () => {
+  try {
+    const subscribers = await getSubscribers();
+    console.log(subscribers);
 
-        // Create a nodemailer transporter for sending emails
-        const transporter = nodemailer.createTransport({
-            service: process.env.EMAIL_SERVICE,
-            auth: {
-                user: process.env.EMAIL_USERNAME,
-                pass: process.env.EMAIL_PASSWORD
-            },
-        });
+    // Create a nodemailer transporter for sending emails
+    const transporter = nodemailer.createTransport({
+      host: "smtp-mail.outlook.com", //marche que avec outlook
+      secureConnection: false,
+      port: 587,
+      tls: {
+        ciphers: "SSLv3",
+      },
+      auth: {
+        user: "teotimetest@outlook.fr",
+        pass: "test_111",
+      },
+    });
 
-        for (const subscriber of subscribers){
-            //remplace les variables de email.html
-            let personalizedContent = newsletterContent
-            .replace('{{lastname}}', subscriber.lastname)
-            .replace('{{firstname}}', subscriber.firstname)
-            
-            // créer le mail
-            const options = {
-                from: process.env.EMAIL_SENDER,
-                to: subscriber.email,
-                subject: "Test newsletter",
-                html: personalizedContent
-            }
+    for (const subscriber of subscribers) {
+      //remplace les variables de email.html
+      let personalizedContent = newsletterContent
+        .replace("{{lastname}}", subscriber.lastname)
+        .replace("{{firstname}}", subscriber.firstname);
 
-            // envoi du mail
-            transporter.sendMail(options, (error, info) =>{
-                if(error) console.log(error)
-                else console.log(info.response)
-            })
+      // images en PJ car sinon ne s'affichent pas
+      const attachments = [
+        {
+          filename: 'building.png',
+          path: path.join(__dirname,'..', 'public/images/building.png'),
+          cid: 'image1', // Use the same 'cid' as in the src attribute
+        },
+        {
+          filename: 'building2.png',
+          path: path.join(__dirname,'..', 'public/images/building2.png'),
+          cid: 'image2', // Use the same 'cid' as in the src attribute
+        },
+      ];
+
+      // créer le mail
+      const options = {
+        from: "teotimetest@outlook.fr",
+        to: subscriber.email,
+        subject: "Test newsletter",
+        html: personalizedContent,
+        attachments: attachments // Attach images
+      };
+
+      // envoi du mail
+      transporter.sendMail(options, (error, info) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log("email envoyé" + info.response);
         }
-    }catch(err){
-        console.error('Error fetching subscribers to populate variables in Newsletter', err)    }
-})()
+      });
+    }
+  } catch (err) {
+    console.error(
+      "Error fetching subscribers to populate variables in Newsletter",
+      err
+    );
+  }; // Add random delay between 1-2 seconds
+})();
 
 router.get('/', (req, res)=>{
     // Send the HTML email template as a response
